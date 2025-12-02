f4875259f1c334bdca45b45deabe06b5f190ac04
fidhasherif55 <sheriffidha633@gmail.com>
2025-12-02 15:54:54 +0530

    Add files via upload

---
+++ program2.php
@@ -0,0 +1,20 @@
+<?php
+
+$students=array("alfee","abhinav","pinky","fahad","nivin");
+
+echo "<h2>Array Before Sorting:</h2>";
+print_r($students);
+
+echo "<br><br>";
+
+asort($students);
+echo "<h2>Array After Ascending sorting(asort):</h2>";
+print_r($students);
+
+echo "<br><br>";
+
+arsort($students);
+echo "<h2>Array After Descending Sorting(arsort):</h2>";
+print_r($students);
+
+?>
