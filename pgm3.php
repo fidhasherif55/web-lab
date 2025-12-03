9a1faec0d34714f8e8224d1eabf0d283fdf43faf
fidhasherif55 <sheriffidha633@gmail.com>
2025-12-03 14:39:14 +0530

    Add files via upload

---
+++ pgm3.php
@@ -0,0 +1,21 @@
+<?php
+
+$students = array("Alfeena", "Nooha", "Fidha");
+
+echo "<h2>Array Before Sorting:</h2>";
+print_r($students);
+
+echo "<br><br>";
+
+asort($students);
+echo "<h2>Array After Ascending Sorting (asort):</h2>";
+print_r($students);
+
+echo "<br><br>";
+
+arsort($students);
+echo "<h2>Array After Descending Sorting (arsort):</h2>";
+print_r($students);
+
+?>
+
